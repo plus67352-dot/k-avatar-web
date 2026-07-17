292a4662273859ea64d38b86ece9429d302f41af
unknown <plus67352@gmail.com>
2026-07-17 09:24:33 +0900

    보안강화:환경변수 방식 적용

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlXMqfl-ayMW8V3HRyF03BfjHuvXRtWbk",
-  authDomain: "k-avatar.firebaseapp.com",
-  projectId: "k-avatar",
-  storageBucket: "k-avatar.firebasestorage.app",
-  messagingSenderId: "18276956342",
-  appId: "1:18276956342:web:878c92e064b896299c0e17",
-  measurementId: "G-BJ9TBREGBL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
